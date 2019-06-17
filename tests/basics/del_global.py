7a92ef72bf3c3883bea278ceae781aeff1f40eb9
George Wort <george.wort@arm.com>
2019-06-17 14:24:16 +0100

    Add testing framework to automatically run tests.

---
+++ tests/basics/del_global.py
@@ -0,0 +1,81 @@
+# del global
+
+def do_del():
+    global x
+    del x
+
+x = 1
+print(x)
+do_del()
+try:
+    print(x)
+    print("FAIL")
+    raise SystemExit
+except NameError:
+    pass
+try:
+    do_del()
+    print("FAIL")
+    raise SystemExit
+except SystemExit:
+    raise
+except: # NameError:
+    # FIXME uPy returns KeyError for this
+    pass
+
+# delete globals using a list
+
+a = 1
+del (a,)
+try:
+    print(a)
+    print("FAIL")
+    raise SystemExit
+except NameError:
+    pass
+
+a = 2
+b = 3
+del (a, b)
+try:
+    print(a)
+    print("FAIL")
+    raise SystemExit
+except NameError:
+    pass
+try:
+    print(b)
+    print("FAIL")
+    raise SystemExit
+except NameError:
+    pass
+
+a = 1
+b = 2
+c = 3
+del (a, b, c)
+try:
+    print(a)
+    print("FAIL")
+    raise SystemExit
+except NameError:
+    pass
+try:
+    print(b)
+    print("FAIL")
+    raise SystemExit
+except NameError:
+    pass
+try:
+    print(c)
+    print("FAIL")
+    raise SystemExit
+except NameError:
+    pass
+
+a = 1
+b = 2
+c = 3
+del (a, (b, c))
+
+print("PASS")
---
+++ tests/basics/del_name.py
@@ -0,0 +1,26 @@
+# del name
+
+x = 1
+print(x)
+del x
+try:
+    print(x)
+    print("FAIL")
+    raise SystemExit
+except NameError:
+    pass
+try:
+    del x
+    print("FAIL")
+    raise SystemExit
+except SystemExit:
+    raise
+except: # NameError:
+    # FIXME uPy returns KeyError for this
+    pass
+
+class C:
+    def f():
+        pass
+
+print("PASS")

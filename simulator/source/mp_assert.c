6b5e2826a5ce2267bfa4f1e02db946022adb9d5e
George Wort <george.wort@arm.com>
2019-08-22 12:25:13 +0100

    Try and use cache on reload.

--- simulator/source/mp_assert.c
+++ simulator/source/mp_assert.c
@@ -7,7 +7,7 @@ void mp_assert(bool x) {
         printf("Internal MicroPython error.\r\nSimulator will reset in 10 seconds.\r\n");
         EM_ASM({ console.log("Internal MicroPython error. Simulator will reset in 10 seconds."); });
         emscripten_sleep(10000);
-        EM_ASM({ location.reload(); });
+        EM_ASM({ location.reload(false); });
         emscripten_sleep(10000);
     }
 }
